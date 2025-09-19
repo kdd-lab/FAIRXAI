583d9cf9c63404907b360aa4d139cd52ae908896
Chiara Baraglia <chiara.baraglia.92@gmail.com>
2025-09-19 10:22:32 +0200

    Dataset subclasses

---
+++ fairxai/dataset/image_dataset.py
@@ -0,0 +1,9 @@
+from lore_sa.dataset import Dataset
+
+class ImageDataset(Dataset):
+
+    def __init__(self):
+        Dataset.__init__(self)
+
+    def update_descriptor(self):
+        pass
---
+++ fairxai/dataset/timeseries_dataset.py
@@ -0,0 +1,9 @@
+from lore_sa.dataset import Dataset
+
+class TimeseriesDataset(Dataset):
+
+    def __init__(self):
+        Dataset.__init__(self)
+
+    def update_descriptor(self):
+        pass
