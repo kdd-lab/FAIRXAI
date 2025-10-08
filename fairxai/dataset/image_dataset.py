60d1f5140b2024fe9e6b30ba4d362c916d7d101b
Chiara Baraglia <chiara.baraglia.92@gmail.com>
2025-10-08 17:21:03 +0200

    FIXME added for ImageDataset

--- fairxai/dataset/image_dataset.py
+++ fairxai/dataset/image_dataset.py
@@ -1,9 +1,23 @@
 from lore_sa.dataset import Dataset
+from pandas import DataFrame
+
 
 class ImageDataset(Dataset):
+    # FIXME: Per le immagini non avrò un dataset credo, il descrittore come dovrebbe essere fatto?
 
-    def __init__(self):
-        Dataset.__init__(self)
+    def __init__(self, data: DataFrame, class_name: str = None, categorial_columns:list = None, ordinal_columns:list = None):
 
-    def update_descriptor(self):
+        self.class_name = class_name
+        self.df = data
+
+        # target columns forced to be the last column of the dataset
+        if class_name is not None:
+            self.df = self.df[[x for x in self.df.columns if x != class_name] + [class_name]]
+
+        self.descriptor = {'numeric': {}, 'categorical': {}, 'ordinal': {}}
+
+        # creation of a default version of descriptor
+        self.update_descriptor(categorial_columns=categorial_columns, ordinal_columns=ordinal_columns)
+
+    def update_descriptor(self, categorial_columns:list = None, ordinal_columns:list = None):
         pass
